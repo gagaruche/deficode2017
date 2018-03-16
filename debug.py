29d0551dfecbe5c545bd9cdaca22eb538cc79678
gagaruche <37444705+gagaruche@users.noreply.github.com>
2018-03-16 15:12:00 +0100

    Update debug.py

--- debug.py
+++ debug.py
@@ -10,17 +10,6 @@ elif:
     print ("Dans le turfu")
     
 
-# ERROR 2
-
-classy Person:
-  def __initalize__(self, first_name, last_name):
-    self.first = first_name
-    self.last = lname
-  def speak(self):
-  print("My name is + " self.fname + " " + self.last)
-
-me = Person("Brandon", "Walsh")
-you = Person("Ethan", "Reed")
 
 me.speak()
 you.self.speak
